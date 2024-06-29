8f1c948b418cec22a6397817691922f08ec16ce9
Andrey Marques <andreymarques822@gmail.com>
2024-06-29 20:26:49 -0300

    Correção do caminho dos arquivos js sobre array

---
+++ teoria/array/js/substituir.js
@@ -0,0 +1,8 @@
+const chapeuDePalha = ["Luffy", "Zoro", "Nami", "Ussop", "Sanji", "Robin"];
+
+//função para substituir um elemento do array ou remover elementos em qualquer posição do array 
+
+const tripulaçao = chapeuDePalha.splice(5, 5, "Chopper");
+
+console.log(chapeuDePalha);
+console.log(tripulaçao)
