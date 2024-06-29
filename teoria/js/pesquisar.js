269b2bf8704495e2cfec5d22a14ca3e83eac62b5
Andrey Marques <andreymarques822@gmail.com>
2024-06-29 18:45:23 -0300

    Trabalhando com array funções para pesquisar por elementos

---
+++ teoria/js/pesquisar.js
@@ -0,0 +1,13 @@
+const chapeuDePalha = ["Luffy", "Zoro", "Nami"];
+
+//Pesquisar se um elemento está no array => se o elemento estiver presente retorna true se não estiver retorna false
+
+const inc = chapeuDePalha.includes("Sanji");
+
+console.log(inc);
+
+//Encontrar o indice de um elemento no array => caso encontre retorna o indice se não encontrar retorna -1
+
+const i = chapeuDePalha.indexOf("Chopper");
+
+console.log(i);
