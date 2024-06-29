54fee8303a4fb123c8962d5db016c4ece83d4ded
Andrey Marques <andreymarques822@gmail.com>
2024-06-29 18:32:12 -0300

    Trabalhando com array funções para adicionar elementos

---
+++ teoria/js/adicionar.js
@@ -0,0 +1,13 @@
+const chapeuDePalha = ["Zoro"];
+
+//função para adicionar elementos ao final do array
+
+chapeuDePalha.push("Nami");
+
+console.log(chapeuDePalha);
+
+//função para adicionar elementos no inicio do array
+
+chapeuDePalha.unshift("Luffy");
+
+console.log(chapeuDePalha);
