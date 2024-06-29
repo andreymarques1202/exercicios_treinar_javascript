e6f9c98f1df8649709c9a717c3d41e4a51007e7c
Andrey Marques <andreymarques822@gmail.com>
2024-06-29 20:41:09 -0300

    Trabalhando com array função de iterar elementos do array

---
+++ teoria/array/js/iterar.js
@@ -0,0 +1,6 @@
+const chapeuDePalha = ["Luffy", "Zoro", "Nami", "Ussop", "Sanji", "Chopper", "Robin"];
+
+for(let i = 0; i < chapeuDePalha.length; i++) {
+    const element = chapeuDePalha[i];
+    console.log(element);
+}
