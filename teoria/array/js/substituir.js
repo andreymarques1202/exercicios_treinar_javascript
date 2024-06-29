dfeb7842e2621dc6fd391dc155b7bcd1f4bcc0a7
Andrey Marques <andreymarques822@gmail.com>
2024-06-29 20:40:43 -0300

    Trabalhando com array função de substituir elementos do array

--- teoria/array/js/substituir.js
+++ teoria/array/js/substituir.js
@@ -1,8 +1,9 @@
-const chapeuDePalha = ["Luffy", "Zoro", "Nami", "Ussop", "Sanji", "Robin"];
+const chapeuDePalha = ["Luffy", "Zoro", "Nami", "Ussop", "Sanji","Robin"];
+
 
 //função para substituir um elemento do array ou remover elementos em qualquer posição do array 
 
-const tripulaçao = chapeuDePalha.splice(5, 5, "Chopper");
+const tripulaçao = chapeuDePalha.splice(5, 1, "Chopper");
 
 console.log(chapeuDePalha);
 console.log(tripulaçao)
