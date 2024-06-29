50ac3cc5ef3ccafe38570972013213f476ee854d
Andrey Marques <andreymarques822@gmail.com>
2024-06-29 18:38:19 -0300

    Trabalhando com array funções para remover elementos

---
+++ teoria/js/remover.js
@@ -0,0 +1,13 @@
+const chapeuDePalha = ["Luffy", "Zoro", "Nami"];
+
+//função para remover o ultimo elemento do array
+
+chapeuDePalha.pop();
+
+console.log(chapeuDePalha);
+
+//função para remover o primeiro elemento do array
+
+chapeuDePalha.shift();
+
+console.log(chapeuDePalha);
