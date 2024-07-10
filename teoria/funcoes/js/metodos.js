7664709f0d8ffa85ffd73887862986438e6dc1b6
Andrey Marques <andreymarques822@gmail.com>
2024-07-10 19:55:07 -0300

    Utilizando metodos de objeto no javascript

---
+++ teoria/funcoes/js/metodos.js
@@ -0,0 +1,11 @@
+const chapeuDePalha = {
+    nome: "Luffy",
+    funcao: "Capitão",
+    lutar() {
+        console.log(this.nome + " Lutou");
+    }
+}
+
+console.log(chapeuDePalha);
+
+chapeuDePalha.lutar();
