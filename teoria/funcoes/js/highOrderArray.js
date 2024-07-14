52a1ceb5b62000b60a38e01b6b77e9b385777f91
Andrey Marques <andreymarques822@gmail.com>
2024-07-14 20:49:27 -0300

    Estrutura do exemplo da teoria de high order function com array

---
+++ teoria/funcoes/js/highOrderArray.js
@@ -0,0 +1,12 @@
+const onePiece = [
+    {nome: "Luffy", akumaNoMi: true, funcao: "Capitão", recompensa: "300 milhões de beris"},
+    {nome: "Zoro", akumaNoMi: false, funcao: "Espadachim", recompensa: "120 milhões de beris"},
+    {nome: "Franky", akumaNoMi: false, funcao: "Carpinteiro", recompensa: "75 milhões de beris"},
+    {nome: "Sanji", akumaNoMi: false, funcao: "Cozinheiro", recompensa: "50 milhões de beris"},
+    {nome: "Robin", akumaNoMi: true, funcao: "Arqueologa", recompensa: "80 milhões de beris"},
+    {nome: "Nami", akumaNoMi: false, funcao: "Navegadora", recompensa: "40 milhões de beris"},
+    {nome: "Ace", akumaNoMi: true, funcao: "???", recompensa: "???"},
+    {nome: "Brook", akumaNoMi: true, funcao: "Musico", recompensa: "???"},
+    {nome: "Chopper", akumaNoMi: true, funcao: "Médico", recompensa: "50 beris"},
+    {nome: "Ussop", akumaNoMi: false, funcao: "Atirador", recompensa: "30 milhões de beris"},
+]
